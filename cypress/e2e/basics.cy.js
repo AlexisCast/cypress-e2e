2ea7a50a35e37e225176a99d70e5a88af61765b4
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-05 18:37:25 -0600

    feat: selecting elements

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,5 +1,16 @@
-describe("template spec", () => {
-	it("passes", () => {
-		cy.visit("https://example.cypress.io");
+/// <reference types="Cypress"/>
+
+describe("tasks page", () => {
+	it("should render the main image", () => {
+		cy.visit("http://localhost:5176/");
+		cy.get(".main-header img");             // nesting
+		//cy.get(".main-header").find("img");     // find must be after 'get', same thing as line 6
+	});
+
+	it("should display the page tile", () => {
+		cy.visit("http://localhost:5176/");
+		cy.get("h1").should("have.length", 1);
+		cy.get("h1").contains("My Cypress Course Tasks");
+		// cy.contains("My Cypress Course Tasks");
 	});
 });
