19c5dd8833d75b5c3dec810c042b190c31ec08e6
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-06 16:50:59 -0600

    feat: sharing logic with test hooks

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -2,9 +2,24 @@
 
 describe("contact form", { defaultCommandTimeout: 4000 }, () => {
 	//for suite config
+	before(() => {
+		// runs only once, before all tests
+	});
+
+	beforeEach(() => {
+		// runs before every test
+		cy.visit("/about");
+	});
+	afterEach(() => {
+		// runs after every test
+	});
+	after(() => {
+		// runs after all tests, only once
+	});
+
 	it("should submit the form", { defaultCommandTimeout: 10000 }, () => {
 		// per test config
-		cy.visit("/about");
+		// cy.visit("/about");
 		cy.get('[data-cy="contact-input-message"]').type("Hello world!");
 		cy.get('[data-cy="contact-input-name"]').type("John Doe");
 		cy.get('[data-cy="contact-btn-submit"]').then((el) => {
@@ -26,7 +41,7 @@ describe("contact form", { defaultCommandTimeout: 4000 }, () => {
 	});
 
 	it("should validate the form input", () => {
-		cy.visit("/about");
+		// cy.visit("/about");
 		cy.get('[data-cy="contact-btn-submit"]').click();
 		cy.get('[data-cy="contact-btn-submit"]').then((el) => {
 			expect(el).to.not.have.attr("disabled");
