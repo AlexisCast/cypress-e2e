d2334bf9b720f6669c57c68697e26957784b2b22
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-06 03:36:10 -0600

    test: should submit the form with chaining

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -18,4 +18,20 @@ describe("contact form", () => {
 			"disabled"
 		);
 	});
+
+	it("should submit the form, with chaining and 'and'", () => {
+		cy.visit("http://localhost:5176/about");
+		cy.get('[data-cy="contact-input-message"]').type("Hello World");
+		cy.get('[data-cy="contact-input-name"]').type("Baz Buzz");
+		cy.get('[data-cy="contact-input-email"]').type("test@hotmail.com");
+		cy.get('[data-cy="contact-btn-submit"]')
+			.contains("Send Message")
+			.and("not.have.attr", "disabled"); // or use 'shouldd()'
+		cy.get('[data-cy="contact-btn-submit"]').click();
+		cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
+		cy.get('[data-cy="contact-btn-submit"]').should(
+			"have.attr",
+			"disabled"
+		);
+	});
 });
