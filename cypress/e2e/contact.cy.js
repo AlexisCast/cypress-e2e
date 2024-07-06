d693b114d00b6b4686160096917e7fb27e3ce7ff
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-06 03:42:06 -0600

    test: should submit the form with chaining, and, values/alieses

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -34,4 +34,18 @@ describe("contact form", () => {
 			"disabled"
 		);
 	});
+
+	it("should submit the form, with chaining, 'and', values/Alieses", () => {
+		cy.visit("http://localhost:5176/about");
+		cy.get('[data-cy="contact-input-message"]').type("Hello World");
+		cy.get('[data-cy="contact-input-name"]').type("Baz Buzz");
+		cy.get('[data-cy="contact-input-email"]').type("test@hotmail.com");
+		cy.get('[data-cy="contact-btn-submit"]')
+			.contains("Send Message")
+			.and("not.have.attr", "disabled"); // or use 'should()'
+		cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+		cy.get("@submitBtn").click();
+		cy.get("@submitBtn").contains("Sending...");
+		cy.get("@submitBtn").should("have.attr", "disabled");
+	});
 });
