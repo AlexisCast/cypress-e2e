7d5924957242ccaf942752b8d295c4f737135f81
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-06 15:33:50 -0600

    test: validate the form input

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -86,4 +86,35 @@ describe("contact form", () => {
 		cy.get("@submitBtn").contains("Sending...");
 		cy.get("@submitBtn").should("have.attr", "disabled");
 	});
+
+	it("should validate the form imput", () => {
+		cy.visit("http://localhost:5176/about");
+		cy.get('[data-cy="contact-btn-submit"]').click();
+		cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+			expect(el).to.not.have.attr("disabled");
+			expect(el.text()).to.not.equal("Sending...");
+		});
+
+		cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
+		cy.get('[data-cy="contact-input-message"]').focus().blur();
+		cy.get('[data-cy="contact-input-message"]')
+			.parent()
+			.then((el) => {
+				expect(el.attr("class")).to.contain("invalid");
+			});
+
+		cy.get('[data-cy="contact-input-email"]').focus().blur();
+		cy.get('[data-cy="contact-input-email"]')
+			.parent()
+			.then((el) => {
+				expect(el.attr("class")).to.contain("invalid");
+			});
+
+		cy.get('[data-cy="contact-input-name"]').focus().blur();
+		cy.get('[data-cy="contact-input-name"]')
+			.parent()
+			.then((el) => {
+				expect(el.attr("class")).to.contain("invalid");
+			});
+	});
 });
