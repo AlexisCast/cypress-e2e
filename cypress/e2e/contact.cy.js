0cac28fceb513b29f7b9f24d94981ba3898b6592
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-06 03:48:54 -0600

    test: should submit the form with chaining, and, values/alieses, then()

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -48,4 +48,22 @@ describe("contact form", () => {
 		cy.get("@submitBtn").contains("Sending...");
 		cy.get("@submitBtn").should("have.attr", "disabled");
 	});
+
+	it("should submit the form, with chaining, 'and', values/Alieses, then()", () => {
+		cy.visit("http://localhost:5176/about");
+		cy.get('[data-cy="contact-input-message"]').type("Hello World");
+		cy.get('[data-cy="contact-input-name"]').type("Baz Buzz");
+		cy.get('[data-cy="contact-input-email"]').type("test@hotmail.com");
+		cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+			expect(el.attr("disabled")).to.be.undefined;
+			expect(el.text()).to.eq("Send Message");
+		});
+		// cy.get('[data-cy="contact-btn-submit"]')
+		// 	.contains("Send Message")
+		// 	.should("not.have.attr", "disabled"); // or use 'and()'
+		cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+		cy.get("@submitBtn").click();
+		cy.get("@submitBtn").contains("Sending...");
+		cy.get("@submitBtn").should("have.attr", "disabled");
+	});
 });
