217f781ebf2875a3add8fd9839610492bcfbe3f0
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-07 02:00:38 -0600

    test: use interceptors

---
+++ cypress/e2e/newsletter_Intercepters.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe("Newsletter", () => {
+	beforeEach(() => {
+		cy.task("seedDatabase");
+	});
+
+	it("should display a success message", () => {
+		cy.intercept("POST", "/newsletter*", {
+			status: 201,
+		}).as("subscribe"); // intercept any http request localhost:3000/newsletter?anything
+		cy.visit("/");
+		cy.get('[data-cy="newsletter-email"]').click();
+		cy.get('[data-cy="newsletter-email"]').type("test@example.com");
+		cy.get('[data-cy="newsletter-submit"]').click();
+		cy.wait("@subscribe"); // not necessay but it is great practice to know that the next line depends on the interceptor
+		cy.contains("Thanks for signing up");
+	});
+});
