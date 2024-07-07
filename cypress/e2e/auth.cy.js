4cf290813313e80dc457306d3a8ba1cc32fb63c4
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-07 16:05:33 -0600

    test: should logout

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -26,4 +26,19 @@ describe("Auth", () => {
 		cy.location("pathname").should("eq", "/takeaways");
 		cy.getCookie("__session").its("value").should("not.be.empty");
 	});
+
+	it("should logout", () => {
+		cy.visit("/login");
+		cy.get('[data-cy="auth-email"]').click();
+		cy.get('[data-cy="auth-email"]').type("test@example.com"); // user exist beacuse of seed-test.js
+		cy.get('[data-cy="auth-password"]').click();
+		cy.get('[data-cy="auth-password"]').type("testpassword");
+		cy.get('[data-cy="auth-submit"]').click();
+		cy.location("pathname").should("eq", "/takeaways");
+		cy.getCookie("__session").its("value").should("not.be.empty");
+
+		cy.contains("Logout").click();
+		cy.location("pathname").should("eq", "/");
+		cy.getCookie("__session").its("value").should("be.empty");
+	});
 });
