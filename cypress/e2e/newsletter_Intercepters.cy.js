c814eaa382816d1e3c242deec1eda9cc85bd36cf
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-07-07 02:07:31 -0600

    test: shoul display validation error

--- cypress/e2e/newsletter_Intercepters.cy.js
+++ cypress/e2e/newsletter_Intercepters.cy.js
@@ -16,4 +16,16 @@ describe("Newsletter", () => {
 		cy.wait("@subscribe"); // not necessay but it is great practice to know that the next line depends on the interceptor
 		cy.contains("Thanks for signing up");
 	});
+
+	it("should display validation error", () => {
+		cy.intercept("POST", "/newsletter*", {
+			message: "Email exist already.",
+		}).as("subscribe"); // intercept any http request localhost:3000/newsletter?anything
+		cy.visit("/");
+		cy.get('[data-cy="newsletter-email"]').click();
+		cy.get('[data-cy="newsletter-email"]').type("test@example.com");
+		cy.get('[data-cy="newsletter-submit"]').click();
+		cy.wait("@subscribe"); // not necessay but it is great practice to know that the next line depends on the interceptor
+		cy.contains("Email exist already.");
+	});
 });
